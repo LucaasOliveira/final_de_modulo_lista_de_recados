091ac07ae656aa625df181a8151c3b9652cc294b
Lucas Almeida <oalmeidaluccas@gmail.com>
2024-07-22 19:21:01 -0300

    feat: alterando validacoes

--- src/middlewares/validate.js
+++ src/middlewares/validate.js
@@ -1,55 +1,50 @@
-import { users } from '../routes/userRoutes';
+import { users } from '../routes/userRoutes.js';
 
-//verifica se os campos obrigatórios estão presentes
+// verificar se os campos obrigatórios estão presentes
 function validateFields(fields, res) {
     for (const [key, value] of Object.entries(fields)) {
         if (!value) {
-            return res.status(400).send(`Por favor, verifique se passou o ${key}.`);
-        };
-    };
-};
+            res.status(400).send(`Por favor, verifique se passou o ${key}.`);
+            return true;
+        }
+    }
+    return false;
+}
 
-// Valida o registro de usuário
 export function validateUserRegistration(req, res, next) {
     const { name, email, password } = req.body;
-    const user = users.find(user => user.email === email);
+    
+    if (validateFields({ nome: name, email: email, senha: password }, res)) return;
 
-    const error = validateFields({ nome: name, email: email, senha: password }, res);
-    if (error) return;
+    const user = users.find(user => user.email === email);
 
     if (user) {
         return res.status(400).send('Email já cadastrado, insira outro.');
-    };
+    }
 
     next();
-};
+}
 
-// Valida o login de usuário
 export function validateUserLogin(req, res, next) {
     const { email, password } = req.body;
 
-    const error = validateFields({ email: email, senha: password }, res);
-    if (error) return;
+    if (validateFields({ email: email, senha: password }, res)) return;
 
     next();
-};
+}
 
-// Valida a criação de uma nova mensagem
 export function validateCreateMessage(req, res, next) {
     const { title, description } = req.body;
 
-    const error = validateFields({ título: title, descrição: description }, res);
-    if (error) return;
+    if (validateFields({ título: title, descrição: description }, res)) return;
 
     next();
-};
+}
 
-// Valida a atualização de uma mensagem existente
 export function validateUpdateMessage(req, res, next) {
     const { title, description } = req.body;
 
-    const error = validateFields({ título: title, descrição: description }, res);
-    if (error) return;
+    if (validateFields({ título: title, descrição: description }, res)) return;
 
     next();
-};
+}
